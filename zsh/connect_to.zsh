2fcf904da8f9d72dbab435abb779422a7168a0bb
da99 <i-hate-spam-1234567@mailinator.com>
2017-06-25 05:19:08 -0500

    Changed: tmux_setup -> my_tmux

--- zsh/connect_to.zsh
+++ zsh/connect_to.zsh
@@ -1,7 +1,7 @@
 
 connect_to () {
   local name=$1; shift
-  session_file="/apps/tmux_setup/config/$name.tmux"
+  session_file="/apps/my_tmux/config/$name.tmux"
   if [[ -f "$session_file" ]]; then
     tmux new-session -A -s "$name" \; source-file "$session_file"
   else
